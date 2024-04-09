972611fc68fbb9b47e4a233ae4868fd41c1a9e22
Endree <endree.epg@gmail.com>
2024-04-09 09:18:00 +0200

    Initial Commit

---
+++ Dota2 Update Notifier/Dota2_Update_NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2_Update_NotifierApp.swift
+//  Dota2 Update Notifier
+//
+//  Created by user on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dota2_Update_NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
