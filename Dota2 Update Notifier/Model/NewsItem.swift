8d7385800f6b9053792a4693683c29ef97fafc67
Endree Eph <fiadotsyeu@gmail.com>
2024-04-09 11:15:58 +0200

    The model elements have been added

--- Dota2 Update Notifier/Model/NewsItem.swift
+++ Dota2 Update Notifier/Model/NewsItem.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+struct NewsItem: Codable {
+    var id: UUID
+    var title: String
+    var leadIn: String
+    var date: Date
+    var content: String
+    var url: String
+    var imageURL: URL
+    var language: String
+}
