a22b0bbabb2b0f272cd3a740b7944e49e6bee62e
DreF <166485147+fiadotsyeu@users.noreply.github.com>
2024-04-14 17:22:15 +0200

    FavoritesView has been added

---
+++ Dota2 Update Notifier/View/FavoritsView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritsView.swift
+//  Dota2 Update Notifier
+//
+//  Created by user on 14.04.2024.
+//
+
+import SwiftUI
+
+struct FavoritsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritsView()
+}
