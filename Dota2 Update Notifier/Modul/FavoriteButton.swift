b37203e0a0dd94831bf029bcc558172e8e4786c4
DreF <166485147+fiadotsyeu@users.noreply.github.com>
2024-04-15 20:53:59 +0200

    Fix the text bug

--- Dota2 Update Notifier/Modul/FavoriteButton.swift
+++ Dota2 Update Notifier/Modul/FavoriteButton.swift
@@ -14,7 +14,7 @@ struct FavoriteButton: View {
         Button {
             isSet.toggle()
         } label: {
-            Image(isSet ? "Favorits.fill" : "Favorits")
+            Image(isSet ? "Favorites.fill" : "Favorites")
                 .resizable()
                 .frame(width: 25, height: 25)
                 .colorMultiply(isSet ? .red : .gray)
