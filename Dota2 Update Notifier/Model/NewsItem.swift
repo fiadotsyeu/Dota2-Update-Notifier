8940ff42d481f7e9b5ccad7d1ed3fc5cec081ae9
DreF <166485147+fiadotsyeu@users.noreply.github.com>
2024-04-20 18:51:06 +0200

    Added a temporary ID generator :)

--- Dota2 Update Notifier/Model/NewsItem.swift
+++ Dota2 Update Notifier/Model/NewsItem.swift
@@ -7,16 +7,31 @@
 
 import Foundation
 
+var counter = 0
+
 struct NewsItem: Codable, Identifiable, Hashable {
-    var id: Int
+    var id: Int?
     var title: String
-    var leadIn: String
     var date: String
     var content: String
-    var url: URL
-    var imageURL: URL
-    var language: String
+    var url: URL?
+    var imageURL: URL?
     var isFavorite: Bool
+    var tag: String
     
+    init(title: String, date: String, content: String, url: URL? = nil, imageURL: URL? = nil, isFavorite: Bool, tag: String) {
+        self.title = title
+        self.date = date
+        self.content = content
+        self.url = url
+        self.imageURL = imageURL
+        self.isFavorite = isFavorite
+        self.tag = tag
+        self.id = generateUniqueInt()
+    }
     
+    private func generateUniqueInt() -> Int {
+        counter += 1
+        return counter
+    }
 }
