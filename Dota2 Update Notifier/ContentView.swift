2ac9c168a2c2a01c157f5edd390de84b540a9aaa
DreF <166485147+fiadotsyeu@users.noreply.github.com>
2024-04-21 16:47:52 +0200

    We load data upon application launch.

--- Dota2 Update Notifier/ContentView.swift
+++ Dota2 Update Notifier/ContentView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(ModelData.self) var modelData
+    let rssURL = URL(string: "https://store.steampowered.com/feeds/news/app/570/?cc=CZ&l=english&snr=1_2108_9__2107")!
+    
     var body: some View {
+        let rssParser = RSSParser(modelData: modelData)
+        
         NavBarView()
+            .onAppear {
+                rssParser.parseRSS(from: rssURL)
+            }
     }
 }
 
