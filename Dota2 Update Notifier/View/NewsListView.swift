086099e241fadb49eded38cda28f14145b45a1e5
DreF <166485147+fiadotsyeu@users.noreply.github.com>
2024-04-22 08:25:25 +0200

    Filters for News, Patches, The International, and All news have been added.

--- Dota2 Update Notifier/View/NewsListView.swift
+++ Dota2 Update Notifier/View/NewsListView.swift
@@ -9,32 +9,46 @@ import SwiftUI
 
 struct NewsListView: View {
     @Environment(ModelData.self) var modelData
-    @State private var showInternationalOnly = false
+    @State private var selectedOptionFilter = 0
+    
     let rssURL = URL(string: "https://store.steampowered.com/feeds/news/app/570/&l=english&snr=1_2108_9__2107")!
         
-    var filteredNewsItems: [NewsItem] {
-        modelData.newsItems.filter { newsItem in
-            (!showInternationalOnly || newsItem.tag.contains("international"))
+    func applyFilter(filteredBy: Int) -> [NewsItem] {
+        switch filteredBy {
+        case 0:
+            return modelData.newsItems // Returning all news without filtering
+        case 1:
+            return modelData.newsItems.filter { $0.tag.contains("News") }
+        case 2:
+            return modelData.newsItems.filter { $0.tag.contains("Patch") }
+        default:
+            return modelData.newsItems.filter { $0.tag.contains("The International") }
         }
     }
-        
+
+
     var body: some View {
         let _rssParser = RSSParser(modelData: modelData)
         
         NavigationView {
             List {
                 Section(header: Text("All news and patches")) {
-                    Toggle(isOn: $showInternationalOnly) {
-                        Text("The International only")
+                    Picker(selection: $selectedOptionFilter, label: Text("Select a filter")) {
+                        Text("All").tag(0)
+                        Text("News").tag(1)
+                        Text("Patches").tag(2)
+                        Text("The International").tag(3)
                     }
-                    ForEach(filteredNewsItems) { newsItem in
+                    .pickerStyle(DefaultPickerStyle())
+                    
+                    ForEach(applyFilter(filteredBy: selectedOptionFilter)) { newsItem in
                         NavigationLink(destination: NewsDetail(newsItem: newsItem)) {
                             NewsRow(newsItem: newsItem)
                         }
                     }
                 }
             }
-            .animation(.default, value: filteredNewsItems)
+            .animation(.default, value: selectedOptionFilter)
             .navigationTitle("Dota Updates Notifier")
         }
     }
