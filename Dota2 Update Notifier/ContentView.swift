3c3b6b28af388af0aeddcf54c4d8fd898671d236
DreF <fiadotsyeu@gmail.com>
2024-04-30 11:55:55 +0200

    Permission check added upon application launch

--- Dota2 Update Notifier/ContentView.swift
+++ Dota2 Update Notifier/ContentView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     @Environment(ModelData.self) var modelData
-    let rssURLs = [URL(string: "https://www.dotabuff.com/blog.rss"), 
+    let rssURLs = [URL(string: "https://www.dotabuff.com/blog.rss"),
                    URL(string: "https://store.steampowered.com/feeds/news/app/570/l=english")]
     
     var body: some View {
@@ -19,6 +20,13 @@ struct ContentView: View {
             .onAppear {
                 rssURLs.forEach { url in
                     rssParser.parseRSS(from: url!)
+                }
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("Permission approved!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
                 }
             }
     }
