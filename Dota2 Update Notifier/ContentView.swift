1fa73b6763bdd737c6b5208a270652c6e6b8674e
DreF <fiadotsyeu@gmail.com>
2024-05-01 09:02:57 +0200

    Background data update implemented every 10 minutes (preliminary)

--- Dota2 Update Notifier/ContentView.swift
+++ Dota2 Update Notifier/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UserNotifications
+import BackgroundTasks
 
 struct ContentView: View {
     @Environment(ModelData.self) var modelData
@@ -18,9 +19,6 @@ struct ContentView: View {
         
         NavBarView()
             .onAppear {
-                rssURLs.forEach { url in
-                    rssParser.parseRSS(from: url!)
-                }
                 UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
                     if success {
                         print("Permission approved!")
@@ -28,7 +26,56 @@ struct ContentView: View {
                         print(error.localizedDescription)
                     }
                 }
+                for url in rssURLs {
+                    Task {
+                        do {
+                            await rssParser.parseRSS(from: url!)
+                            print("date: \(Date()), sync rss parser starting")
+                        }
+                    }
+                }
             }
+    }
+
+    init() {
+        registerBackgroundTask()
+    }
+        
+    private func registerBackgroundTask() {
+        let backgroundTaskIdentifier = "DreF.Dota2-Update-Notifier.backgroundTask"
+        
+        BGTaskScheduler.shared.register(
+            forTaskWithIdentifier: backgroundTaskIdentifier,
+            using: nil
+        ) { task in
+            self.handleBackgroundTask(task: task as! BGProcessingTask)
+        }
+        
+        let request = BGAppRefreshTaskRequest(identifier: backgroundTaskIdentifier)
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 10 * 60) // "Every 10 minutes."
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print("Error registering background task: \(error)")
+        }
+    }
+        
+    private func handleBackgroundTask(task: BGProcessingTask) {
+        let rssParser = RSSParser(modelData: modelData)
+        
+        for url in rssURLs {
+            Task {
+                do {
+                    await rssParser.parseRSS(from: url!)
+                    print("date: \(Date()), sync rss parser starting")
+                    task.setTaskCompleted(success: true)
+                } catch {
+                    print("Error parsing RSS: \(error)")
+                    task.setTaskCompleted(success: false)
+                }
+            }
+        }
     }
 }
 
