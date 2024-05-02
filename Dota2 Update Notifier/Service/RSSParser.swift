3690147ee7b4e63e18409a36281723023e55583c
DreF <fiadotsyeu@gmail.com>
2024-05-02 14:33:20 +0200

    Fixed a bug when changing the language that caused the function to not understand the date format and return nil

--- Dota2 Update Notifier/Service/RSSParser.swift
+++ Dota2 Update Notifier/Service/RSSParser.swift
@@ -82,7 +82,7 @@ class RSSParser: NSObject, XMLParserDelegate {
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         if elementName == "item" {
-            let newsItem = NewsItem(title: currentTitle, date: formatDate(currentPubDate) ?? "No data", content: currentDescription, url: URL(string: currentLink), imageURL: URL(string: currentImgURL), isFavorite: false, tag: filterNewsItemsByTag(title: currentTitle))
+            let newsItem = NewsItem(title: currentTitle, date: formatDate(currentPubDate), content: currentDescription, url: URL(string: currentLink), imageURL: URL(string: currentImgURL), isFavorite: false, tag: filterNewsItemsByTag(title: currentTitle))
             
             if !dublicateFinder(title: newsItem.title) {
                 modelData.newsItems.append(newsItem)
@@ -123,15 +123,16 @@ class RSSParser: NSObject, XMLParserDelegate {
         }
     }
     
-    func formatDate(_ dateString: String) -> String? {
+    func formatDate(_ dateString: String) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss Z"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "E, dd MMM yyyy HH:mm:ss Z"
         
         if let date = dateFormatter.date(from: dateString) {
             dateFormatter.dateFormat = "dd MMMM yyyy, HH:mm"
             return dateFormatter.string(from: date)
         } else {
-            return nil
+            return "No date"
         }
     }
 
